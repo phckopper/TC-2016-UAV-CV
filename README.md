bf1644d928ed4dfba1cfe9356b35e7018860bdff
Pedro Henrique Kopper <pedrohkopper@gmail.com>
2016-03-10 20:45:29 -0300

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# TC - 2016 - (NO NAME YET)
+
+Repo created for storing code related to my 2016 final paper on image processing and UAVs
---
+++ video_capture.py
@@ -0,0 +1,24 @@
+# Code downloaded from https://opencv-python-tutroals.readthedocs.org/en/latest/py_tutorials/py_gui/py_video_display/py_video_display.html
+# For reference only, I didn't write it
+
+import numpy as np
+import cv2
+
+cap = cv2.VideoCapture(1) # Select secondary video stream, we don't want the built-in webcam
+
+while(True):
+    # Capture frame-by-frame
+    ret, frame = cap.read()
+
+    # Our operations on the frame come here
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+    # Display the resulting frame
+    cv2.imshow('frame',gray)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+# When everything done, release the capture
+cap.release()
+cv2.destroyAllWindows()
+
